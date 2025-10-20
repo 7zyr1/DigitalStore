cd6f84777319b203b5b1ea4956cba59106af34e0
7zyr1 <valenmellia48@gmail.com>
2025-10-20 15:04:31 -0300

    genericRepo

---
+++ DigitalStoreModel/DigitalStorePersistance/repositories/implementations/GenericRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DigitalStoreModel.Models.Core;
+using DigitalStorePersistance.repositories.interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace DigitalStorePersistance.repositories.implementations
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly DigitalStoreDBContext _context;
+        protected readonly DbSet<T> _dbSet;
+        public GenericRepository(DigitalStoreDBContext context)
+        {
+            _dbSet = context.Set<T>();
+            _context = context;
+        }
+
+        public virtual void AddOne(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public virtual void Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public virtual async Task<List<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public virtual async Task<T?> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public virtual void Update(T entity)
+        {
+            _dbSet.Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+    }
+}
---
+++ DigitalStoreModel/DigitalStorePersistance/repositories/interfaces/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalStorePersistance.repositories.interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        void AddOne(T entity);
+        Task<T?> GetByIdAsync(int id);
+        Task<List<T>> GetAllAsync();
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
